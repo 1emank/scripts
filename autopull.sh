a289447622fe85a0e131bbba0cd5b95d973bc0a2
Carlos Andrés González Bono <carlos.a.gb95@gmail.com>
2024-07-27 20:17:35 +0200

    Simpler logging, impoved scripts

--- autopull.sh
+++ autopull.sh
@@ -1,14 +1,105 @@
 #!/bin/bash
-#cd "the absolute path to the folder with your repositories" # So you don't
-# need to pass arguments or to be in a specific folder.
+pulled_something=false
+didnt_pull_message="You didn't pull from any repository"
+norepos_message="ERROR: Something went wrong and no repositories were found."
+help_message="
 
-# To avoid unwanted behavior, make sure that the output of "ls" won't have
-# any unwanted files or directories, either having the appropiate folder
-# structure, or modifying the next line with the appropiate arguments
-repos=($(ls))
+This scripts pulls the changes from your git repositories.
 
-for repo in "${repos[@]}"; do
-    cd ${repos[i]}
-    git pull origin main
-    cd ..
+To use this script you need to have a folder with all (or the intended)
+repositories. Optionally, put this script there directly too:
+I.E.: repos_folder
+        ├repo1  
+        ├repo2 (it can be a symbolic link too)
+        ├...
+        └autopull.sh (optional)
+
+And then:
+- If you put the script next to your repository folders, just run it
+- If not, run the script giving the appropiate route as an argument
+    - I.E.: ./autopull.sh route/to/repos_folder
+
+You can specify the remote with -r|--remote \"remote name or path\" and
+the branch with -b|--branch \"branch name\". If not specified, remote
+will be \"origin\", and branch will be \"main\". Defaults can be set
+from inside the script.
+"
+remote="origin"
+branch="main"
+
+if ! options=$(getopt -o r:b: --long remote:,branch: -n "Options" -- "$@")
+then
+    echo "Invalid options provided"
+    exit 1
+fi
+
+eval set -- "$options"
+while true; do
+    case "$1" in
+    -h|--help)
+        echo "$help_message"
+        exit 0 ;;
+    -r|--remote)
+        remote="$2"
+        shift 2 ;;
+    -b|--branch)
+        branch="$2"
+        shift 2 ;;
+    --)
+        shift
+        break ;;
+    *) 
+        echo "Unknown error"
+        exit 1 ;;
+    esac
 done
+
+if [ -n "$1" ]
+then
+    route="$(readlink -f "$1")"
+else
+    route="$(dirname "$(readlink -f "$0")")"
+fi
+
+printf "\nLooking for repositories inside: %s\n\n" "$route"
+
+readarray -d '' content < <(find "$route" -not -path "$route/*/*" -a -not -path "$route" -print0)
+repos=()
+
+for item in "${content[@]}"
+do
+    if [ ! -d "$item" ]
+    then
+        continue
+    fi
+    
+    if git -C "$item" status 2&> /dev/null
+    then
+        repos+=("$item")
+        printf "Repository found: %s\n" "$item"
+    fi
+done
+
+if [ ${#repos[@]} -eq 0 ]
+then
+    echo "$norepos_message$help_message"
+    exit 1
+fi
+
+for repo in "${repos[@]}"
+do
+    echo
+    echo "Pulling from $repo:"
+    if git -C "$repo" pull "$remote" "$branch"
+    then
+        pulled_something=true
+    fi
+done
+
+if ! $pulled_something
+then
+    echo "$didnt_pull_message"
+    exit 2
+fi
+
+exit 0
--- update.sh
+++ update.sh
@@ -1,89 +1,83 @@
 #!/bin/bash
-out=false
-# Default behaviour gives you a log in /var/log/custom_logs
-# To have a log in "~/out" use the -o|--out flag or, uncomment
-# the next line
-#out=true
-
-options=$(getopt -o o --long out -n "Options" -- "$@")
-if [ ! $? = 0 ]; then
-    echo "Invalid options provided"
-    exit 1
+if [ "$(whoami)" != "root" ]
+then
+	echo "This script must be run as root"
+	exit 1
 fi
 
-eval set -- "$options"
-while true; do
-    case "$1" in
-    -o|--out) out=true; shift ;;
-    --) shift ; break ;;
-    *) echo "Unknown error" ; exit 1 ;;
+help_message="This script updates the packages of your linux system.
+
+To run this script just run it as administrator. There's no need to pass
+any arguments.
+
+By default, a log is stored in \"/var/log/1emank/update.log\", you can
+change this by editing the \"default_route\" variable of the script.
+"
+# To change the log location, modify the next variable
+default_route="/var/log/1emank/update.log"
+
+log_dir="$(dirname "$default_route")"
+log_file="$(basename "$default_route")"
+
+for arg in "$@"
+do
+    case "$arg" in
+        -h|--help)
+            echo "$help_message"
+            exit 0 ;;
     esac
 done
 
-if [ ! `whoami` = root ]; then
-	echo "This script must be run as root"
-	exit 0
-fi
+mkdir -p "$log_dir"
+exec > >(tee "$log_dir/$log_file") 2>&1
 
-if [ "${SUDO_USER,,}" != "root" ] && [ ! -z "$SUDO_USER" ] && $out ; then
-    mkdir -p "/home/${SUDO_USER,,}/out"
-    exec > >(tee "/home/${SUDO_USER,,}/out/update.log") 2>&1
-else
-    mkdir -p "/var/log/custom_logs"
-    exec > >(tee "/var/log/custom_logs/update.log") 2>&1
-fi
+###### ------------------------- BEGIN ------------------------- ######
+printf "\n### Auto-update started: %s ###\n" "$(date)"
 
-printf "\n### Auto-update started: `date` ###\n"
-
-which apt-get > /dev/null
-if [ $? -eq 0 ]; then
+if which apt-get > /dev/null
+then
     printf "## apt update ##\n"
     apt-get update
 fi
 
-which apt > /dev/null
-if [ $? -eq 0 ]; then
+if which apt > /dev/null
+then
     printf "\n## apt upgrade ##\n"
-    temp_var=$(echo `apt list --upgradable 2> /dev/null | grep / | cut -d "/" -f1 | tr "\n" " "`)
-    if [ ! -z "$temp_var" ]; then
-        apt-get install $temp_var -y
+    temp_var="$(apt list --upgradable 2> /dev/null | grep / | cut -d "/" -f1 | tr "\n" " ")"
+    if [ -n "$temp_var" ]; then
+        apt-get install "$temp_var" -y
     else
         echo "Nada que actualizar en apt"
     fi
 fi
 
-which apt-get > /dev/null
-if [ $? -eq 0 ]; then
+if which apt-get > /dev/null
+then
     printf "\n## apt autoremove ##\n"
     apt-get autoremove -y --purge
 fi
 
-which snap > /dev/null
-if [ $? -eq 0 ]; then
+if which snap > /dev/null
+then
     printf "\n## snap refresh ##\n"
     snap refresh
     snap changes | tail -2
 fi
 
-which flatpak > /dev/null
-if [ $? -eq 0 ]; then
-    printf  "\n## flatpak update ##\n"
+if which flatpak > /dev/null
+then
+    printf "\n## flatpak update ##\n"
     flatpak update -y
 fi
 
-which canonical-livepatch > /dev/null
-if [ $? -eq 0 ]; then
+if which canonical-livepatch > /dev/null
+then
     printf "\n## canonical-livepatch refresh ##\n"
     canonical-livepatch refresh
 fi
 
-printf "\n### Auto-update finished: `date` ###\n"
-if [ "${SUDO_USER,,}" != "root" ] && [ ! -z "$SUDO_USER" ] && $out ; then
-    echo "-----See log in ~/out/update.log-----"
-    printf "\n\n"
-else
-    echo "---See log in /var/log/custom_logs/update.log---"
-    printf "\n\n"
-fi
+printf "\n### Auto-update finished: %s ###\n" "$(date)"
+echo "-----See log in $log_dir/$log_file-----"
+printf "\n\n"
 
 exit 0
