b7a82cb5ef318b17e6c5ef5b30cc60170d4ae1aa
1emank <carlos.a.gb95@gmail.com>
2024-10-30 20:21:21 +0100

    update.sh: Better logs

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 help_message="This script updates the packages of your linux system.
 
-It uses the appropiate commands to update the packages of your package managers (apt, flatpak, etc). A log is stored in \"/var/log/1emank/update.log\". Run this script as administrator.
+    It uses the appropiate commands to update the packages of your package managers (apt, flatpak, etc). Run this script as administrator. 
 
     Options:
-./update.sh [ -h | --help ]
-"
+. update.sh [ -h | --help ]
+
+    A log is stored in \"/var/log/1emank/update.log\". Set up log rotation manually or use the \"1emank_logrorate.sh\" script from this repository."
+
 log_route="/var/log/1emank/update.log"
 
 case "$1" in
@@ -18,7 +20,7 @@ esac
 
 if [ "$(whoami)" != "root" ]
 then
-    echo "This script must be run as root. See help with the --help flag"
+    echo "This script must be run as root. See help with --help."
     exit 1
 fi
 
@@ -26,39 +28,44 @@ mkdir -p "$(dirname "$log_route")"
 exec > >(tee -a "$log_route") 2>&1
 
 ###### ------------------------- BEGIN ------------------------- ######
-printf "\n### Auto-update started: %s ###\n" "$(date)"
+printf "### START %s\n" "$(date)"
 
 if which apt-get > /dev/null
 then
-    printf "\n## apt update, full-upgrade and cleanup ## (using apt-get)\n"
-    apt-get update
-    apt-get dist-upgrade -y
-    apt-get autoremove -y --purge
-    apt-get clean
+    echo "# apt #"
+    apt_out="$(\
+      apt-get -U dist-upgrade -yqq | tee /dev/tty;
+      apt-get autoremove -yqq --purge | tee /dev/tty;
+      apt-get clean | tee /dev/tty \
+    )"
+    if [ -z "$apt_out" ]; then
+        echo "Nothing changed."
+    fi
 fi
 
 if which snap > /dev/null
 then
-    printf "\n## snap refresh ##\n"
+    echo "# snap #"
     snap refresh
     snap changes | tail -2
 fi
 
 if which flatpak > /dev/null
 then
-    printf "\n## flatpak update and cleanup ##\n"
-    flatpak update -y
-    flatpak uninstall --unused --delete-data -y
+    echo "# flatpak #"
+    printf "Update result:"
+    flatpak update --noninteractive -y
+    flatpak uninstall --noninteractive --unused --delete-data -y
     rm -rfv /var/tmp/flatpak-cache-*
 fi
 
 if which canonical-livepatch > /dev/null
 then
-    printf "\n## canonical-livepatch refresh ##\n"
+    printf "# canonical-livepatch #"
     canonical-livepatch refresh
 fi
 
-printf "\n### Auto-update finished: %s ###\n\n" "$(date)"
-printf "See log in: %s\n\n" "$log_route"
+printf "### END %s\n" "$(date)"
+printf "\nSee log in: %s\n\n" "$log_route" > /dev/tty
 
 exit 0
